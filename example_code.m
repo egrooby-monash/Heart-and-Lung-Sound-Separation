c0b4f7780291766bbbd5e82d13ad0104cad2fa63
egrooby-monash <90371852+egrooby-monash@users.noreply.github.com>
2022-08-14 17:38:48 -0700

    Update example_code.m

--- example_code.m
+++ example_code.m
@@ -73,7 +73,7 @@ options_nmf.W6=0.25;
 options_nmf.beta_loss=1;
 options_nmf.sparsity=0.1;
 MAXITER = 100;
-K=[20 10 20 20 20];
+K=[20 10 20 20 20 20];
 
 [V_h, V_l, V_n,V_s, V_r, W_1, W_3, W_4, W_5, W_6]=...
     load_example2(respiratory_support, TF,options_tf, max_examples,heart_path,lung_path,cry_path,stmv_path,bubble_path,cpap_path,options_nmf,MAXITER,K);
@@ -113,7 +113,7 @@ options_nmf.W6=0.25;
 options_nmf.beta_loss=1;
 options_nmf.sparsity=0.1;
 MAXITER = 100;
-K=[20 10 20 20 20];
+K=[20 10 20 20 20 20];
 
 [V_h, V_l, V_n,V_s, V_r, W_1, W_3, W_4, W_5, W_6]=...
     load_example2(respiratory_support, TF,options_tf, max_examples,heart_path,lung_path,cry_path,stmv_path,bubble_path,cpap_path,options_nmf,MAXITER,K);
